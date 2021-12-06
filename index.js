782d903e87e03fe738b82e8ece451c2a3b5fcff9
RhythmiCon <conormcguigan.code@gmail.com>
2021-12-06 16:09:32 +0000

    task started

---
+++ index.js
@@ -0,0 +1,28 @@
+let myCollection = [
+    {
+      name: "School of Code mug",
+      count: 1,
+      whatILike: "It has my cute pixel character on it!"
+    },
+    {
+      name: "School of Code hat",
+      count: 2,
+      whatILike: "An often overlooked fashion accessory"
+    },
+    {
+      name: "School of Code pillow",
+      count: 1,
+      whatILike: "Eat. Sleep. Code. Repeat :)"
+    }
+  ];
+
+  console.log(myCollection);
+
+function describeItem(item){
+    item = myCollection.name
+    console.log(item.count)
+}
+
+
+
+console.log(describeItem("School of Code mug"))
